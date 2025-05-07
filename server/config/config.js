3da5247d3e043f31881f989f2ad68d410e272b7a
dayan gajadeera <dayanweerasekara5@gmail.com>
2025-05-07 09:20:53 +0530

    mongo atals added

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     port: process.env.PORT || 3000,
     mongoose: {
-        url: process.env.MONGODB_URI || 'mongodb://localhost:27017/your-db-name'
+        url: process.env.MONGODB_URI
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3ZjliYjM2YzZjMmRlNGRmZWUwMjI2ZiIsInJvbGUiOiJ0ZWNobmljaWFuIiwiaWF0IjoxNzQ0NDIwMTkxLCJleHAiOjE3NDQ1MDY1OTF9.w_EbR-C4bRHa2JBxgR2Xi_mHNqd_J9u0N8FaLWiYg-A',
